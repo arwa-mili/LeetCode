5eec13b926ed08131065e94ccf6f13618f39f549
rw@@arwa <64184160+arwa-mili@users.noreply.github.com>
2025-04-17 23:45:42 +0100

    Time: 11 ms (3.76%), Space: 18.8 MB (5.15%) - LeetHub

--- 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
+++ 2277-count-equal-and-divisible-pairs-in-an-array/2277-count-equal-and-divisible-pairs-in-an-array.cpp
@@ -1,17 +1,48 @@
+//by mahdi
 class Solution {
 public:
     int countPairs(vector<int>& nums, int k) {
-        int res = 0;
+        int ans = 0;
         int n = nums.size();
-        
-        for (int i = 0; i < n; ++i) {
-            for (int j = i + 1; j < n; ++j) {
-                if (nums[i] == nums[j] && (i * j) % k == 0) {
-                    res++;
-                }
+        vector<int> cnt(n+1,0);
+        vector<pair<int,int> > v;
+        vector<int> divs[n+1];
+        for(int i=1;i<=n;i++){
+            for(int j=i;j<=n;j+=i){
+                divs[j].push_back(i);
             }
         }
-
-        return res;
+        for(int i=0;i<n;i++){
+            v.push_back({nums[i],i});
+        }
+        sort(v.begin(),v.end());
+        int l=0;
+        for(int i=0;i<n;){
+            int j = i;
+            while(j<n && v[j].first==v[i].first){
+                j++;
+            }
+            j--;
+            for(int o=i;o<=j;o++){
+                if(v[o].second==0){
+                    ans+=j-i;
+                    continue;
+                }
+                int d = __gcd(v[o].second, k);
+                d = k/d;
+                if(d<n)ans+=cnt[d];
+                for(auto &di:divs[v[o].second]){
+                    cnt[di]++;
+                }
+            }
+            for(int o=i;o<=j;o++){
+                if(v[o].second==0)continue;
+                for(auto &di:divs[v[o].second]){
+                    cnt[di]--;
+                }
+            }
+            i=j+1;
+        }
+        return ans;
     }
 };
