4ee0bbbc6ea2b963440c339f3a76f1006cad763e
rw@@arwa <64184160+arwa-mili@users.noreply.github.com>
2025-08-03 10:12:58 +0100

    Time: 0 ms (100%), Space: 9.8 MB (11.17%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+
+        vector<vector<int>> v;
+        v.push_back({1}); 
+        int k = 1;
+        while(k<numRows) {
+            vector<int> elem;
+            elem.push_back({1}); 
+            for (int i =1;i< k;i++){
+                
+                elem.push_back(v[k - 1][i - 1] + v[k - 1][i]); 
+            }
+            elem.push_back(1);
+            v.push_back(elem); 
+            k += 1;
+           
+        }
+        return v;
+        
+    }
+};
