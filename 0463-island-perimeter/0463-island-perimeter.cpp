857f10f918c283098bd26508e585b1837b339840
rw@@arwa <64184160+arwa-mili@users.noreply.github.com>
2025-08-10 22:17:05 +0100

    Time: 15 ms (22.51%), Space: 107.9 MB (12.92%) - LeetHub

--- 0463-island-perimeter/0463-island-perimeter.cpp
+++ 0463-island-perimeter/0463-island-perimeter.cpp
@@ -1,26 +1,35 @@
+#include <vector>
+using namespace std;
+
 class Solution {
 public:
     vector<vector<int>> grid;
     vector<vector<bool>> visited;
-    int m,n;
+    int m, n;
+
     int dfs(int i, int j) {
-        if (i < 0 || j < 0 || i == m || j == n || !grid[i][j]) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 0)
             return 1;
-        }
-        if (visited[i][j]) return 0;
-        visited[i][j] = 1;
-        return dfs(i+1, j) + dfs(i-1, j) + dfs(i, j+1) + dfs(i, j-1);
+        
+        if (visited[i][j]) 
+            return 0;
+        
+        visited[i][j] = true;
+
+        return dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j + 1) + dfs(i, j - 1);
     }
 
     int islandPerimeter(vector<vector<int>>& grid) {
-        m = grid.size();    
-        n = grid[0].size();    
+        m = grid.size();
+        n = grid[0].size();
         this->grid = grid;
-        visited = vector<vector<bool>> (m, vector<bool> (n, false));
-        for (int i = 0; i<m; i++) {
-            for (int j = 0; j<n; j++) {
-                if (grid[i][j]) {
-                    return dfs(i,j);
+
+        visited.assign(m, vector<bool>(n));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    return dfs(i, j);
                 }
             }
         }
