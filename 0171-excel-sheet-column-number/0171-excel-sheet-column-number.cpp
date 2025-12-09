626055cf0806ccb00372b5baefd88d8bd46ce9c0
rw@@arwa <mili.arwa.ma@gmail.com>
2025-12-09 20:54:29 +0100

    Time: 0 ms (100%), Space: 8.5 MB (93.6%) - LeetHub

--- 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
+++ 0171-excel-sheet-column-number/0171-excel-sheet-column-number.cpp
@@ -1,20 +1,12 @@
 class Solution {
 public:
     int titleToNumber(string columnTitle) {
-
-        map<char,int> m = {
-            {'A', 1}, {'B', 2}, {'C', 3}, {'D', 4}, {'E', 5},
-            {'F', 6}, {'G', 7}, {'H', 8}, {'I', 9}, {'J', 10},
-            {'K', 11}, {'L', 12}, {'M', 13}, {'N', 14}, {'O', 15},
-            {'P', 16}, {'Q', 17}, {'R', 18}, {'S', 19}, {'T', 20},
-            {'U', 21}, {'V', 22}, {'W', 23}, {'X', 24}, {'Y', 25},
-            {'Z', 26}
-        };
-
-        int ans = 0;
-        for (char c : columnTitle) {
-            ans = ans * 26 + m[c];
+        int result = 0;
+        for(char c : columnTitle)
+        {
+            int d = c - 'A' + 1;
+            result = result * 26 + d;
         }
-        return ans;
+        return result;
     }
 };
