7c06ac6c678c045aa04931079cc034b513c40951
Arwa Mili <mili.arwa.ma@gmail.com>
2026-02-13 23:17:27 +0100

    Time: 262 ms (19.21%), Space: 27.1 MB (28.67%) - LeetHub

---
+++ 3713-longest-balanced-substring-i/3713-longest-balanced-substring-i.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int longestBalanced(string s) {
+        unordered_map<char, int> mp;
+        int sz = s.size(), ans = 0;
+        
+        for(int i = 0; i < sz; i++) {
+            mp = {}; 
+            
+            for(int j = i; j < sz; j++) {
+                mp[s[j]]++;
+                
+                bool flag = true;
+                int cnt = mp[s[j]];  
+                
+                for(auto x : mp) {
+                    if(x.second != cnt) {
+                        flag = false;
+                        break;
+                    }
+                }
+                
+                if(flag) {
+                    ans = max(ans, j - i + 1);
+                }
+            }
+        }
+        
+        return ans;
+    }
+};
