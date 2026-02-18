a80a3b731a6a3493e9f6e3c7fc0aabf0dd0948aa
Arwa Mili <mili.arwa.ma@gmail.com>
2026-02-18 23:59:45 +0100

    Time: 0 ms (100%), Space: 11.3 MB (24.5%) - LeetHub

--- 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
+++ 2073-time-needed-to-buy-tickets/2073-time-needed-to-buy-tickets.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     int timeRequiredToBuy(vector<int>& tickets, int k) {
 
-        queue<pair<int,int>> q;
-
-        for (int i = 0; i < tickets.size(); i++) {
-            q.push({tickets[i], i});
+        queue<int> q;
+        int res =0;
+        for (int i =0; i< tickets.size(); i++) {
+            q.push(i);
         }
 
-        int total = 0;
-
-        while (!q.empty()) {
-
-            auto current = q.front();
+        while (tickets[k] > 0) {
+            int fr = q.front();
             q.pop();
-
-            current.first--;
-            total++;
-
-            if (current.second == k && current.first == 0) {
-                return total;
+            tickets[fr]--;
+            res++;
+            if (tickets[fr]) {
+               q.push(fr);
             }
 
-            if (current.first > 0) {
-                q.push(current);
-            }
+
         }
-
-        return total;
+        return res;
+        
+        
     }
 };
